e9dde0040dfa0bfad8a33bf2b45dd063153f2ef7
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-20 09:09:23 -0300

    creating file index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cread, Update, Delete PHP MySQL</title>
+  <link rel="stylesheet" type="text/css" href="/assets/css/style">
+</head>
+<body>
+
+<form method="post" action="server.php">
+
+  <div class="input-group">
+    <label>Name</label>
+    <input type="text" name="name" value="">
+  </div>
+  <div class="input-group">
+    <label>Addres</label>
+    <input type="text" name="address" value="">
+  </div>
+  <div class="input-group">
+    <button class="btn" type="submit" name="save">Save</button>
+  </div>
+
+</form>
+
+</body>
+</html>
