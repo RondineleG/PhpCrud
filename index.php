26bc842a47969726cb50c70286c4a92d10f7fd7b
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-20 12:57:31 -0300

    modify button save for also update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('server.php');
 
 if (isset($_GET['edit'])) {
@@ -6,7 +6,7 @@ if (isset($_GET['edit'])) {
 	$update = true;
 	$record = mysqli_query($db, "SELECT * FROM phpcrud.developer WHERE id=$id");
 
-	if (count($record) == 1 ) {
+	if (count($record) == 1) {
 		$n = mysqli_fetch_array($record);
 		$name = $n['name'];
 		$description = $n['description'];
@@ -16,63 +16,70 @@ if (isset($_GET['edit'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>CRUD: PHP and MySQL </title>
 	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
+
 <body>
-	<?php if (isset($_SESSION['message'])): ?>
+	<?php if (isset($_SESSION['message'])) : ?>
 		<div class="msg">
-			<?php 
-				echo $_SESSION['message']; 
-				unset($_SESSION['message']);
+			<?php
+			echo $_SESSION['message'];
+			unset($_SESSION['message']);
 			?>
 		</div>
 	<?php endif ?>
 
-<?php $results = mysqli_query($db, "SELECT * FROM phpcrud.developer"); ?>
+	<?php $results = mysqli_query($db, "SELECT * FROM phpcrud.developer"); ?>
 
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>Description</th>
-			<th colspan="2">Action</th>
-		</tr>
-	</thead>
-	
-	<?php while ($row = mysqli_fetch_array($results)) { ?>
-		<tr>
-			<td><?php echo $row['name']; ?></td>
-			<td><?php echo $row['description']; ?></td>
-			<td>
-				<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-			</td>
-			<td>
-				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
-			</td>
-		</tr>
-	<?php } ?>
-</table>
-	
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Description</th>
+				<th colspan="2">Action</th>
+			</tr>
+		</thead>
+
+		<?php while ($row = mysqli_fetch_array($results)) { ?>
+			<tr>
+				<td><?php echo $row['name']; ?></td>
+				<td><?php echo $row['description']; ?></td>
+				<td>
+					<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn">Edit</a>
+				</td>
+				<td>
+					<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+				</td>
+			</tr>
+		<?php } ?>
+	</table>
 
 
-<form method="post" action="server.php" >
-// newly added field
-<input type="hidden" name="id" value="<?php echo $id; ?>">
 
-// modified form fields
-	<div class="input-group">
-		<label>Name</label>
-		<input type="text" name="name" value="<?php echo $name; ?>">
-	</div>
-	<div class="input-group">
-		<label>Description</label>
-		<input type="text" name="description" value="<?php echo $description; ?>">
-	</div>
-	<div class="input-group">
-			<button class="btn" type="submit" name="save" >Save</button>
+	<form method="post" action="server.php">
+		// newly added field
+		<input type="hidden" name="id" value="<?php echo $id; ?>">
+
+		// modified form fields
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="<?php echo $name; ?>">
 		</div>
-</form>
+		<div class="input-group">
+			<label>Description</label>
+			<input type="text" name="description" value="<?php echo $description; ?>">
+		</div>
+		<div class="input-group">
+			<?php if ($update == true) : ?>
+				<button class="btn" type="submit" name="update" style="background: #556B2F;">update</button>
+			<?php else : ?>
+				<button class="btn" type="submit" name="save">Save</button>
+			<?php endif ?>
+		</div>
+	</form>
 </body>
+
 </html>
