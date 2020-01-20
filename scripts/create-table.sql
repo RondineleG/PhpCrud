3d812c126adc1b02574b2a61443dc7d3d2201445
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-20 09:00:52 -0300

    script for creating table

---
+++ scripts/create-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS info (
+     id     INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR (100)        DEFAULT NULL,
+    address  VARCHAR (400)        DEFAULT NULL
+);
